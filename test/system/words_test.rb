f3f95f944eef120702933662548d218b573ae76b
Yi Jie Ng ngyijie92@gmail.com <ngyijie92@gmail.com>
2021-11-11 18:51:16 +0800

    Write test cases

---
+++ test/system/words_test.rb
@@ -0,0 +1,12 @@
+require "application_system_test_case"
+# require_relative "../../app/controllers/games_controller"
+
+class WordsTest < ApplicationSystemTestCase
+  # test "saying word not in grid yields a sorry message" do
+  #   visit new_url
+  #   fill_in "word", with: "BI"
+  #   click_on "Play"
+
+  #   assert_text "Sorry but BI can't be built out of #{@letters.join(', ')}."
+  end
+end
